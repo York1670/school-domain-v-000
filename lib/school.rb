d9c8062a1a56b71f65e9ba6e77d97937a71eb172
Bobbi York <york1670@gmail.com>
2018-11-02 05:07:03 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -12,9 +12,6 @@ class School
 
   def add_student(student_name, grade)
     @roster[grade] = []
-    if not @roster[grade].include?(grade)
-      @roster = [grade]
-    end
     @roster[grade] << student_name
   end
 
