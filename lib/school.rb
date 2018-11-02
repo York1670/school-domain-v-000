28277a513cee728431fb32c3cb3545c19396513d
Bobbi York <york1670@gmail.com>
2018-11-02 04:37:25 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,25 @@
-# code here!
+class School
+
+  attr_accessor :student_name, :grade
+  attr_reader :school, :roster
+
+  school = School.new
+
+  def initialize(school)
+    @school = school
+    @roster = roster
+  end
+
+  def roster
+    roster = {}
+  end
+
+  def add_student(student_name, grade)
+    roster[grade] = []
+    roster[grade] << student_name
+  end
+
+  def grade(grade)
+    puts roster[grade]
+  end
+end
