011484adfc9100845b6bd498db80a3e502eb2397
Bobbi York <york1670@gmail.com>
2018-11-02 04:59:51 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -11,11 +11,11 @@ class School
   end
 
   def add_student(student_name, grade)
-    roster[grade] = []
-    roster[grade] << student_name
+    @roster[grade] = []
+    @roster[grade] << student_name
   end
 
   def grade(grade)
-    puts roster[grade]
+    
   end
 end
